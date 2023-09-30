1d02ab7f55eb9d623670f7cace395f4f5cfaec18
Anderson Silva <joseandersonsilv4@gmail.com>
2023-09-30 18:11:11 -0300

    adicionando produtos att, incluindo o detalhamento e quantidade de produtos

--- templates/static/js/scripts.js
+++ templates/static/js/scripts.js
@@ -1,4 +1,5 @@
 
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Toggle the side navigation
@@ -34,3 +35,33 @@ function scrollRight() {
     let container = document.getElementById('scrollableArea');
     container.scrollLeft += 200; // ajuste este valor conforme necessário
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var produtos = document.querySelectorAll('.card');
+    var quantidadeDisplay = document.getElementById('quantidadeDisplay');
+    var adicionarBtn = document.getElementById('adicionarQuantidade');
+
+    adicionarBtn.addEventListener('click', function() {
+        var currentQuantity = parseInt(quantidadeDisplay.innerText, 10);
+        quantidadeDisplay.innerText = currentQuantity + 1;
+    });
+
+    produtos.forEach(function(produto) {
+        produto.addEventListener('click', function() {
+            var nome = this.querySelector('.card-title').innerText;
+            var descricao = this.querySelector('.card-text').innerText;
+            var imagemSrc = this.querySelector('.card-img-top').src;
+
+            // Reset the quantity display to 1
+            quantidadeDisplay.innerText = '1';
+
+            document.getElementById('produtoNome').innerText = nome;
+            document.getElementById('produtoDescricao').innerText = descricao;
+            document.getElementById('produtoImagem').src = imagemSrc;
+
+            var modal = new bootstrap.Modal(document.getElementById('produtoModal'));
+            modal.show();
+        });
+    });
+});
